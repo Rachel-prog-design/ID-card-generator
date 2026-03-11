be886cf593a5e2ef45ee7057bbd4e1c9ee1ffd25
Rachel-prog-design <rachelnyinawabana7@gmail.com>
2026-03-11 17:12:29 +0200

    cards

--- id-card/src/components/ProfileCard.jsx
+++ id-card/src/components/ProfileCard.jsx
@@ -1,6 +1,6 @@
 function ProfileCard({ image, name, role }) {
   return (
-    <div className="bg-white text-black p-4 rounded shadow text-center">
+    <div className="text-black p-4 rounded shadow text-center">
       <img
         src={image}
         alt={name}
@@ -9,7 +9,7 @@ function ProfileCard({ image, name, role }) {
       <h2 className="mt-2 font-bold">{name}</h2>
       <p>{role}</p>
     </div>
-  );
+     );
 }
 
 export default ProfileCard;
