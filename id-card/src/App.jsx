d91d1ed2eee4aa9e45150f899e782acfd47385d5
Rachel-prog-design <rachelnyinawabana7@gmail.com>
2026-03-30 17:34:50 +0200

    some upates

--- id-card/src/App.jsx
+++ id-card/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-green-300 gap-6">
       <IDCard name="Rachel Nyinawabana" role="Student" id="KC23173" />
-      <IDCard name="Gad Johnson" role="Developer" id="KC23174" />
+      <IDCard name="Gad Mugisha" role="Developer" id="KC23174" />
       <IDCard name="Bob Smith" role="Designer" id="KC23175" />
     </div>
   );
